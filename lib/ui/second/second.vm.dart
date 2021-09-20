7eb5f1999b9cd873370949291ad099c365e110df
Christian Neil Anthony Wico <cnawico@gmail.com>
2021-09-20 16:55:48 +0800

    Wire navigators into existing viewmodels

--- lib/ui/second/second.vm.dart
+++ lib/ui/second/second.vm.dart
@@ -1,5 +1,11 @@
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:stacked_starter/app/app.locator.dart';
 
 class SecondViewModel extends BaseViewModel {
-  void returnToStartupPage() {}
+  final NavigationService _navigator = locator<NavigationService>();
+
+  void returnToStartupPage() {
+    _navigator.back();
+  }
 }
--- lib/ui/startup/startup.vm.dart
+++ lib/ui/startup/startup.vm.dart
@@ -1,5 +1,12 @@
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:stacked_starter/app/app.locator.dart';
+import 'package:stacked_starter/app/app.router.dart';
 
 class StartupViewModel extends BaseViewModel {
-  void navigateToSecondPage() {}
+  final NavigationService _navigator = locator<NavigationService>();
+
+  void navigateToSecondPage() {
+    _navigator.navigateTo(Routes.secondView);
+  }
 }
